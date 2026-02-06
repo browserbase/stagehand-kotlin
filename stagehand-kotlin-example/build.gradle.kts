5b983118f1f66d0e2d91ca68bbd9301495f636c7
monadoid <sam.finton@gmail.com>
2026-02-06 10:26:32 -0700

    Fix Kotlin example schema and Playwright dependency.

--- stagehand-kotlin-example/build.gradle.kts
+++ stagehand-kotlin-example/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 dependencies {
     implementation(project(":stagehand-kotlin-core"))
     implementation(project(":stagehand-kotlin-client-okhttp"))
-    implementation("com.microsoft.playwright:playwright:1.58.1")
+    implementation("com.microsoft.playwright:playwright:1.43.0")
 }
 
 application {
